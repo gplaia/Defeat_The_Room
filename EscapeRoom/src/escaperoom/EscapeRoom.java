b24fba5833c33c1eb6ee9633b0fcb52da5324167
Andy Wang <awang0027@gmail.com>
2019-10-06 20:29:48 -0400

    Printing Hello

--- EscapeRoom/src/escaperoom/EscapeRoom.java
+++ EscapeRoom/src/escaperoom/EscapeRoom.java
@@ -17,6 +17,7 @@ public class EscapeRoom {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HELLO WORLD");
+        //Hello
     }
     
 }
